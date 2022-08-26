f13bcf5290a75742e3f277c4ae3e9322feed610a
Nguyen Tuan Dat <datb9a2@gmail.com>
2022-08-26 22:54:54 +0700

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,7 @@
+docker build -t my-tomcat-image .
+docker run -d --name tomcat-container -p 8080:8080 my-tomcat-image
+useradd dockeradmin
+passwd dockeradmin
+usermod -aG docker dockeradmin
+mv ./cosc2767-assignment2-website/sshd_config /etc/ssh/sshd_config
+service sshd reload
